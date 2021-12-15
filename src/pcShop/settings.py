b19fcf76ac98cb824df0d825ae226be4936bfb5e
Dina <dina.maloyarova@mail.ru>
2021-12-15 21:04:31 +0600

    postgresql

--- src/pcShop/settings.py
+++ src/pcShop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-y*#(6bu=9c!x_=kpxo-)ut4v$q7oo8&z^%2c-ng2n6r+yud!q)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
@@ -82,8 +82,17 @@ WSGI_APPLICATION = 'pcShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR/'db.sqllite'
+    # }
     }
 }
 
